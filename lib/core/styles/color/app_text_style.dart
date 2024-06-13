68a6d9722eaf8d2206850d01e7c2d80a9f3704a5
Mohamed sobhy <mohamedyamany@055@gmail.com>
2024-06-13 15:55:08 +0300

    add file text style

---
+++ lib/core/styles/color/app_text_style.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:hafiz_app/core/styles/color/app_color.dart';
+
+abstract class AppTextStyle {
+  static const TextStyle textPoppins20 = TextStyle(
+    color: Colors.white,
+    fontWeight: FontWeight.w400,
+    fontFamily: "Poppins",
+    fontSize: 20,
+  );
+  static const TextStyle textBold = TextStyle(
+    color: AppColor.lightGreen,
+    fontWeight: FontWeight.bold,
+    fontSize: 35,
+  );
+
+  static const TextStyle text14Black = TextStyle(
+    color: Colors.black,
+    fontWeight: FontWeight.w400,
+    fontSize: 14,
+  );
+
+  static const TextStyle textAmiri20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+    color: AppColor.lightGreen,
+    fontFamily: "Amiri",
+  );
+}
