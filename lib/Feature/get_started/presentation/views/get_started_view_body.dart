023dbe4252538d3492bd9a25c52ab373a9d2dbaf
Mohamed sobhy <mohamedyamany@055@gmail.com>
2024-06-13 21:37:42 +0300

    remove unccessary import package

--- lib/Feature/get_started/presentation/views/get_started_view_body.dart
+++ lib/Feature/get_started/presentation/views/get_started_view_body.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hafiz_app/Feature/get_started/presentation/views/get_started_details_view.dart';
 import 'package:hafiz_app/Feature/get_started/presentation/views/get_started_logo_view.dart';
-import 'package:hafiz_app/core/extensions/context_extensions.dart';
-import 'package:hafiz_app/core/styles/color/app_color.dart';
 import 'package:hafiz_app/core/utils/image_assets.dart';
 
 class GetStartedViewBody extends StatelessWidget {
