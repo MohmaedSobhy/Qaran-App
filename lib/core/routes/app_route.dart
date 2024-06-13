c641be8d754461817de0fb63480906869ca29939
Mohamed sobhy <mohamedyamany@055@gmail.com>
2024-06-13 21:55:13 +0300

    add route folder

--- lib/core/routes/app_route.dart
+++ lib/core/routes/app_route.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:hafiz_app/Feature/get_started/presentation/screens/get_started_screen.dart';
+import 'package:hafiz_app/Feature/home/presentation/screens/home_screen.dart';
+import 'package:hafiz_app/core/routes/base_route.dart';
 
-class BaseRoute extends PageRouteBuilder {
-  BaseRoute({required super.pageBuilder})
-      : super(
-          transitionDuration: const Duration(milliseconds: 200),
-          transitionsBuilder: (_, animation, secondaryAnimation, widget) {
-            return SlideTransition(
-              position: Tween<Offset>(
-                begin: const Offset(1, 0),
-                end: Offset.zero,
-              ).animate(animation),
-              child: widget,
-            );
+abstract class AppRoute {
+  static const String getStartedScreen = '/getStarted';
+  static const String homeScreen = '/HomeScreen';
+  static String suraScreen = '/SuraScreen';
+
+  static Route<dynamic>? onGenerateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case getStartedScreen:
+        return MaterialPageRoute(
+          builder: (context) {
+            return const GetStartedScreen();
           },
         );
+      case homeScreen:
+        return BaseRoute(pageBuilder: (_, __, ___) {
+          return const HomeScreen();
+        });
+    }
+    return null;
+  }
 }
--- lib/core/routes/base_route.dart
+++ lib/core/routes/base_route.dart
@@ -1,20 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:hafiz_app/Feature/get_started/presentation/screens/get_started_screen.dart';
 
-abstract class AppRoute {
-  static const String getStartedScreen = '/getStarted';
-  static const String homeScreen = '/HomeScreen';
-  static String suraScreen = '/SuraScreen';
-
-  static Route<dynamic>? onGenerateRoute(RouteSettings settings) {
-    switch (settings.name) {
-      case getStartedScreen:
-        return MaterialPageRoute(
-          builder: (context) {
-            return const GetStartedScreen();
+class BaseRoute extends PageRouteBuilder {
+  BaseRoute({required super.pageBuilder})
+      : super(
+          transitionDuration: const Duration(milliseconds: 200),
+          transitionsBuilder: (_, animation, secondaryAnimation, widget) {
+            return SlideTransition(
+              position: Tween<Offset>(
+                begin: const Offset(1, 0),
+                end: Offset.zero,
+              ).animate(animation),
+              child: widget,
+            );
           },
         );
-    }
-    return null;
-  }
 }
