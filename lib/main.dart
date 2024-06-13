1ebb43adc4ecfdd3c216e4f806ae11c5c7745c36
Mohamed sobhy <mohamedyamany@055@gmail.com>
2024-06-13 13:46:44 +0300

    Intial commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:hafiz_app/qaran_app.dart';
+
+void main() {
+  runApp(const QaranApp());
+}
---
+++ lib/qaran_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class QaranApp extends StatelessWidget {
+  const QaranApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
