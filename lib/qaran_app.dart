e57008b2cbda3a956453f7b6bc1d4506b922be23
Mohamed sobhy <mohamedyamany@055@gmail.com>
2024-06-13 21:55:28 +0300

    on tab in get started button

--- lib/qaran_app.dart
+++ lib/qaran_app.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:hafiz_app/Feature/get_started/presentation/screens/get_started_screen.dart';
+import 'package:hafiz_app/core/routes/app_route.dart';
+import 'package:hafiz_app/core/styles/theme/app_theme.dart';
+import 'package:hafiz_app/core/styles/theme/controller/app_theme_cubit.dart';
 
 class QaranApp extends StatelessWidget {
   const QaranApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return BlocProvider(
+      create: (context) => AppThemeCubit.instanse..checkTheme(),
+      child: BlocBuilder<AppThemeCubit, AppThemeState>(
+        builder: (context, state) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            localizationsDelegates: AppLocalizations.localizationsDelegates,
+            supportedLocales: AppLocalizations.supportedLocales,
+            home: const GetStartedScreen(),
+            onGenerateRoute: AppRoute.onGenerateRoute,
+            initialRoute: AppRoute.getStartedScreen,
+            theme: (state is LightThemeState)
+                ? AppTheme.lightTheme
+                : AppTheme.darkTheme,
+          );
+        },
+      ),
+    );
   }
 }
