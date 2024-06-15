20ba45fb480f838eec1b61b93716c6ec218c1a47
Mohamed sobhy <mohamedyamany@055@gmail.com>
2024-06-15 05:16:45 +0300

    init dio serviese

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hafiz_app/core/api/dio_helper.dart';
 import 'package:hafiz_app/core/helper/storage_helper.dart';
 import 'package:hafiz_app/qaran_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await StorageHelper.init();
+  await DioService.init();
   runApp(const QaranApp());
 }
