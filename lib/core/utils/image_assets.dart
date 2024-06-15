bad13ae3b3e9db0da58957d99d8a81a10ec509c3
Mohamed sobhy <mohamedyamany@055@gmail.com>
2024-06-15 05:17:19 +0300

    make varialbe const

--- lib/core/utils/image_assets.dart
+++ lib/core/utils/image_assets.dart
@@ -1,8 +1,8 @@
 abstract class ImageAssets {
-  static String imagePath = 'assets/images';
+  static const String imagePath = 'assets/images';
 
-  static String imgGroupCircles = '$imagePath/group_circles.svg';
-  static String imgQuranOnboarding = '$imagePath/quran_onboarding.svg';
-  static String imgLastReadBg = '$imagePath/las_read_bg.svg';
-  static String imgBismillah = '$imagePath/bismillah.png';
+  static const String imgGroupCircles = '$imagePath/group_circles.svg';
+  static const String imgQuranOnboarding = '$imagePath/quran_onboarding.svg';
+  static const String imgLastReadBg = '$imagePath/las_read_bg.svg';
+  static const String imgBismillah = '$imagePath/bismillah.svg';
 }
