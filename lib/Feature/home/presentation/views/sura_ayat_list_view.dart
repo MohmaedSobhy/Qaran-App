48b48e931a3c4d26b785ff518728230c2355b4c5
Mohamed sobhy <mohamedyamany@055@gmail.com>
2024-06-15 15:02:50 +0300

    network connection in sura screen

--- lib/Feature/home/presentation/views/sura_ayat_list_view.dart
+++ lib/Feature/home/presentation/views/sura_ayat_list_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hafiz_app/Feature/home/presentation/controller/sura/sura_cubit.dart';
 import 'package:hafiz_app/Feature/home/presentation/widgets/aya_list_view_item.dart';
 import 'package:hafiz_app/core/styles/color/app_color.dart';
+import 'package:hafiz_app/core/widgets/no_internect_connection.dart';
 
 class SuraAyatListView extends StatelessWidget {
   const SuraAyatListView({super.key});
@@ -27,7 +28,7 @@ class SuraAyatListView extends StatelessWidget {
             ),
           );
         } else {
-          return Center();
+          return const NoInternectConnectionView();
         }
       },
     );
