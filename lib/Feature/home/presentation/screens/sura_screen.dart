af029dd05d203c3df6ee0ee0f96ae4b7aa6e3776
Mohamed sobhy <mohamedyamany@055@gmail.com>
2024-06-15 14:46:53 +0300

    add bloc provider in sura screen

--- lib/Feature/home/presentation/screens/sura_screen.dart
+++ lib/Feature/home/presentation/screens/sura_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hafiz_app/Feature/home/presentation/controller/sura/sura_cubit.dart';
 import 'package:hafiz_app/Feature/home/presentation/views/sura_screen_view_body.dart';
 
 class SuraScreen extends StatelessWidget {
@@ -8,9 +10,12 @@ class SuraScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        body: SuraScreenViewBody(
-          suraId: suraId,
+      child: BlocProvider.value(
+        value: SuraCubit.instanse..fetchAyatSura(suraId: suraId),
+        child: Scaffold(
+          body: SuraScreenViewBody(
+            suraId: suraId,
+          ),
         ),
       ),
     );
