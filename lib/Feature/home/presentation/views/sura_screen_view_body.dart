c3268d117bffc230d95189a3d90220d01cb63420
Mohamed sobhy <mohamedyamany@055@gmail.com>
2024-06-15 05:18:18 +0300

    sure body view

---
+++ lib/Feature/home/presentation/views/sura_screen_view_body.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:hafiz_app/Feature/home/presentation/views/sura_name_card_view.dart';
+
+class SuraScreenViewBody extends StatelessWidget {
+  final int suraId;
+  const SuraScreenViewBody({super.key, required this.suraId});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: SuraNameCardView(
+            suradId: suraId,
+          ),
+        ),
+      ],
+    );
+  }
+}
