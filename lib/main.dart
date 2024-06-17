9a9421e7d9b95f7ee18e3e7977eaf83b176825c4
Mohamed sobhy <mohamedyamany@055@gmail.com>
2024-06-17 11:33:54 +0300

    add Future wait

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hafiz_app/core/api/dio_helper.dart';
 import 'package:hafiz_app/core/helper/storage_helper.dart';
 import 'package:hafiz_app/qaran_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await StorageHelper.init();
-  await DioService.init();
+  Future.wait([
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]),
+    DioService.init(),
+    StorageHelper.init(),
+  ]);
   runApp(const QaranApp());
 }
