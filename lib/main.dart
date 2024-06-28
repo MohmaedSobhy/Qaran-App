05fd422396229204bbc26ee9880a2f52e12c578f
Mohamed sobhy <mohamedyamany@055@gmail.com>
2024-06-28 09:38:12 +0300

    app bloc Observal

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hafiz_app/bloc_observer.dart';
 import 'package:hafiz_app/core/api/dio_helper.dart';
 import 'package:hafiz_app/core/helper/storage_helper.dart';
 import 'package:hafiz_app/qaran_app.dart';
@@ -7,6 +9,7 @@ import 'package:hafiz_app/qaran_app.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await StorageHelper.init();
+  Bloc.observer = AppBlocObserval();
   Future.wait([
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
